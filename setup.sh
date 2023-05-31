52cb95c0bd10ac2b449bb8e6146875478eead7f6
Alan Liu <b05901017@ntu.edu.tw>
2023-05-31 22:50:39 +0800

    update

--- setup.sh
+++ setup.sh
@@ -4,6 +4,18 @@ read -r huggingface_token
 read -r key_id
 read -r access_key < "key.txt"
 echo "$huggingface_token\n$key_id\n$access_key"
-wget https://data.commoncrawl.org/crawl-data/CC-MAIN-$SNAPSHOT/wet.paths.gz -O wets/wet_path/$SNAPSHOT
+
+directory="wets/wet_path/$SNAPSHOT"
+
+# Check if directory exists
+if [ -d "$directory" ]; then
+  echo "Directory already exists: $directory"
+else
+  # Create the directory
+  mkdir -p "$directory"
+  echo "Directory created: $directory"
+fi
+
+wget https://data.commoncrawl.org/crawl-data/CC-MAIN-$SNAPSHOT/wet.paths.gz -O wets/wet_path/$SNAPSHOT/wet.paths.gz
 echo -e "$huggingface_token\nn" | huggingface-cli login 
 echo -e "$key_id\n$access_key\n\n\n\n" | aws configure
