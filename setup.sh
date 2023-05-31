320bb946bb62c8e3e9dca445f1a5ae18cbdf3b41
Alan Liu <b05901017@ntu.edu.tw>
2023-05-31 22:57:58 +0800

    update

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ read -r key_id
 read -r access_key < "key.txt"
 echo "$huggingface_token\n$key_id\n$access_key"
 
+echo -e "$huggingface_token\nn" | huggingface-cli login 
+echo -e "$key_id\n$access_key\n\n\n\n" | aws configure
+
 directory="wets/wet_path/$SNAPSHOT"
 
 # Check if directory exists
@@ -16,6 +19,5 @@ else
   echo "Directory created: $directory"
 fi
 
-wget https://data.commoncrawl.org/crawl-data/CC-MAIN-$SNAPSHOT/wet.paths.gz -O wets/wet_path/$SNAPSHOT/wet.paths.gz
-echo -e "$huggingface_token\nn" | huggingface-cli login 
-echo -e "$key_id\n$access_key\n\n\n\n" | aws configure
+aws s3 cp s3://commoncrawl/crawl-data/CC-MAIN-$SNAPSHOT/wet.paths.gz wets/wet_path/$SNAPSHOT/wet.paths.gz
+#wget https://data.commoncrawl.org/crawl-data/CC-MAIN-$SNAPSHOT/wet.paths.gz -O wets/wet_path/$SNAPSHOT/wet.paths.gz
