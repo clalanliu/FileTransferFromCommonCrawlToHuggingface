c69e469f3b7ab362abf1be077a4d56c1488c2deb
Alan Liu <b05901017@ntu.edu.tw>
2023-05-31 22:46:02 +0800

    update setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 SNAPSHOT=$1
+# Read the first three lines from key.txt
+read -r huggingface_token
+read -r key_id
+read -r access_key < "key.txt"
 
-wget https://data.commoncrawl.org/crawl-data/CC-MAIN-2020-05/wet.paths.gz -O wets/wet_path/$SNAPSHOT
-huggingface-cli login
-aws configure
+wget https://data.commoncrawl.org/crawl-data/CC-MAIN-$SNAPSHOT/wet.paths.gz -O wets/wet_path/$SNAPSHOT
+echo -e "$huggingface_token\nn" | huggingface-cli login 
+echo -e "$key_id\n$access_key\n\n\n\n" | aws configure
